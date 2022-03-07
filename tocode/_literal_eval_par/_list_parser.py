4f054937947bc65822430188e2e9db487ca66919
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-03-07 20:02:36 +0330

    change path to _literal_eval_par

--- tocode/_literal_eval_par/_list_parser.py
+++ tocode/_literal_eval_par/_list_parser.py
@@ -79,17 +79,23 @@ class __Str:
         :param _str: list in string
         :return: return encoded string
         """
+        _numeric_flag = False
         _str = list(_str)
         _space_index_st = 0
         for _index in range(len(_str)):
             if _str[_index] == ' ':
-                if _str[_index - 1] not in __inner_signs__ and _str[_index + 1] not in __inner_signs__:
+                if _str[_index - 1] not in __inner_signs__ and _str[_index + 1] not in __inner_signs__ and not _numeric_flag:
                     _str[_index] = 'र'
                     if _space_index_st == 0:
                         _space_index_st = _index
                 else:
                     _str[_index] = ''
             else:
+                if _str[_index] in __inner_signs__:
+                    _numeric_flag = True
+                if (not _str[_index].isnumeric()) and _str[_index] != ' ' and _str[_index] != '' and (_str[_index] not in __inner_signs__):
+                    _numeric_flag = False
+
                 if _space_index_st > 0 and _str[_index] == ',':
                     for _ind in range(_space_index_st, _index):
                         _str[_ind] = ''
