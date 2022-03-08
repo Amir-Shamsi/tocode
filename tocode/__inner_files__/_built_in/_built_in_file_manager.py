f8ddf63d41a3deb71bdeda8ae4aab1e301773c8f
Amir Shamsi <emirshamsi.uni@gmail.com>
2022-03-08 13:06:47 +0330

    path correction for getting `abso path parent`

--- tocode/__inner_files__/_built_in/_built_in_file_manager.py
+++ tocode/__inner_files__/_built_in/_built_in_file_manager.py
@@ -4,11 +4,11 @@ def __reset_files():
     """
      file rester
     """
-    _current_abso_path = pathlib.Path(__file__).parent.absolute().parents[1]
-    _inner_lcode_src = open(_current_abso_path.joinpath('__inner_files__/_src/__code_sr_list__.py'), 'r')
+    _current_abso_path = pathlib.Path(__file__).parent.parent.absolute()
+    _inner_lcode_src = open(_current_abso_path.joinpath('_src/__code_sr_list__.py'), 'r')
     _raw = _inner_lcode_src.read()
     _inner_lcode_src.close()
-    os.remove(_current_abso_path.joinpath('__inner_files__/_gen/__code_gen_list__.py'))
-    _inner_lcode_gen = open(_current_abso_path.joinpath('__inner_files__/_gen/__code_gen_list__.py'), 'w')
+    os.remove(_current_abso_path.joinpath('_gen/__code_gen_list__.py'))
+    _inner_lcode_gen = open(_current_abso_path.joinpath('_gen/__code_gen_list__.py'), 'w')
     _inner_lcode_gen.write(_raw)
     _inner_lcode_gen.close()
